bffe6312860344c611866ec06f73faca6795dd94
MartinLiii <li1924825830@gmail.com>
2026-04-18 15:51:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -598,6 +598,7 @@ def logout():
 # =========================
 # 🚀 启动
 # =========================
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+  if __name__ == '__main__':
+      init_db()
+      port = int(os.environ.get('PORT', 5000))
+      app.run(host='0.0.0.0', port=port, debug=True)
