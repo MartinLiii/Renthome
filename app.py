71140b485a2e3dd0a5b41941f3d8afe95a202b31
MartinLiii <li1924825830@gmail.com>
2026-04-18 15:54:04 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -598,7 +598,7 @@ def logout():
 # =========================
 # 🚀 启动
 # =========================
-  if __name__ == '__main__':
-      init_db()
-      port = int(os.environ.get('PORT', 5000))
-      app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == '__main__':
+    init_db()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
